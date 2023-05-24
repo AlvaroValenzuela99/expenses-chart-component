86b5129ee9e21e8a4e16e00d889c92740a128f6e
Alvaro Valenzuela <alvarovgerena@gmail.com>
2023-05-24 21:56:45 +0200

    creamos la estructura de la pagina y funcion basica para recorrer el json

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const displayExpenses = async (url) => {
     const response = await fetch(url);
     const expenses = await response.json();
 
-    const tabla = document.querySelector(".spending-chart");
-    tabla.innerHTML = "Hola"
 
+    const tabla = document.querySelector(".spending-chart");
+
+    //Recorremos el array elemento por elemento
     expenses.forEach(el => {
-        console.log(el)
-        tabla.a
+
+        //Pintamos la barra y le añadimos su clase
+        let bar = document.createElement("div");
+        bar.className = "daily-bar";
+        tabla.appendChild(bar);
+
+        //Pintamos el día de la semana y le añadimos su clase
+        let day = document.createElement("div");
+        day.innerText += el.day;
+        day.className = "day"
+        tabla.appendChild(day)
         
     });
 
