e15b729ff533738261aa848b31022b73dd121da9
Alvaro Valenzuela <alvarovgerena@gmail.com>
2023-05-25 12:52:22 +0200

    working on the spending chart and JS

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ const displayExpenses = async (url) => {
     //Recorremos el array elemento por elemento
     expenses.forEach(el => {
 
+        let dayExpense = document.createElement("div")
+        dayExpense.className = "dayExpense"
+        tabla.appendChild(dayExpense);
+
         //Pintamos la barra y le añadimos su clase
         let bar = document.createElement("div");
         bar.className = "daily-bar";
-        tabla.appendChild(bar);
+        dayExpense.appendChild(bar);
 
         //Pintamos el día de la semana y le añadimos su clase
         let day = document.createElement("div");
         day.innerText += el.day;
         day.className = "day"
-        tabla.appendChild(day)
-        
-    });
+        dayExpense.appendChild(day)
 
+        let calcHeigth = el.amount * 3
+        bar.style.height = `${calcHeigth}px`
+    });
 }
 
 displayExpenses('data.json')
